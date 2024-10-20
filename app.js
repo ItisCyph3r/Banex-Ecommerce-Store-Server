76a997b7ae6ee9161f3fc8a74652119247a58a40
Momoh Osaze <76972139+ItisCyph3r@users.noreply.github.com>
2024-10-20 21:27:33 +0100

    cloudinary integration

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Routes
+app.use("/", (req, res) => (res.send("Welcome to Banex API")));
 app.use("/api", authRouter);
 app.use("/api/user", usersRouter);
 app.use("/api/category", categoryRouter);
